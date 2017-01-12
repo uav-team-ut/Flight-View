2828cf9569e9ceb6c09e70d1ebd4407b807cabc7
bloof-bb <bloof.bb@gmail.com>
2017-01-12 09:44:26 -0600

    Added new clients to list, added missing semicolons

--- core/net/ipc-server.js
+++ core/net/ipc-server.js
@@ -1,75 +1,74 @@
-const IPC = require('node-ipc').IPC
-const sprintf = require('sprintf-js').sprintf
+'use strict';
 
-const MessageHandler = require('../../util/message-handler')
+const IPC = require('node-ipc').IPC;
+
+const MessageHandler = require('../../util/message-handler');
 
 class IPCServer extends MessageHandler {
     constructor(id) {
-        super()
+        super();
 
-        this._id = id
+        this._id = id;
 
-        this._ipc = new IPC()
-        this._ipc.config.id = id
-        this._ipc.config.silent = true
+        this._ipc = new IPC();
+        this._ipc.config.id = id;
+        this._ipc.config.silent = true;
 
-        this._sockets = []
+        this._sockets = [];
     }
 
     _addEventListeners() {
         this._ipc.server.on('connect', (socket) => {
-            this.emit('connect', socket)
-        })
+            this._sockets.push(socket);
+
+            this.emit('connect', socket);
+        });
 
         this._ipc.server.on('message', (data, socket) => {
-            this.handleMessage(data.message, socket)
+            this.handleMessage(data.message, socket);
 
-            this.emit('receive', data.message, socket)
-        })
+            this.emit('receive', data.message, socket);
+        });
 
         this._ipc.server.on('socket.disconnect', (socket) => {
-            this.emit('disconnect', socket)
-        })
+            this.emit('disconnect', socket);
+        });
 
         this._ipc.server.on('close', () => {
-            this.emit('close')
-        })
+            this.emit('close');
+        });
 
         this._ipc.server.on('error', (e, socket) => {
-            this.emit('error', e, socket)
-        })
+            this.emit('error', e, socket);
+        });
     }
 
     listen() {
         this._ipc.serve(() => {
-            this._addEventListeners()
+            this._addEventListeners();
 
-            this.emit('listening')
-        })
+            this.emit('listening');
+        });
 
-        this._ipc.server.start()
+        this._ipc.server.start();
     }
 
     stopListening() {
-        this._ipc.server.stop()
+        this._ipc.server.stop();
     }
 
     send(socket, message) {
-        this._ipc.server.emit(
-            socket,
-            'message',
-            {
-                id: this._id,
-                message: message
-            }
-        )
+        this._ipc.server.emit(socket, 'message', {
+            id: this._id,
+            message: message
+        });
     }
 
     broadcast(message) {
         for (let i = 0; i < this._sockets.length; i++) {
-            this.send(this._sockets[i], message)
+            this.send(this._sockets[i], message);
         }
     }
 }
 
-module.exports = IPCServer
+module.exports = IPCServer;
--- flight-view/js/ipc-client.js
+++ flight-view/js/ipc-client.js
@@ -1,65 +1,63 @@
-const IPC = require('node-ipc').IPC
-const sprintf = require('sprintf-js').sprintf
+'use strict';
 
-const MessageHandler = require('../../util/message-handler')
+const IPC = require('node-ipc').IPC;
+
+const MessageHandler = require('../../util/message-handler');
 
 class IPCClient extends MessageHandler {
     constructor(id, connectID) {
-        super()
+        super();
 
-        this._id = id
-        this._connectID = connectID
+        this._id = id;
+        this._connectID = connectID;
 
-        this._ipc = new IPC()
-        this._ipc.config.id = id
-        this._ipc.config.silent = true
+        this._ipc = new IPC();
+        this._ipc.config.id = id;
+        this._ipc.config.silent = true;
     }
 
     _addEventListeners() {
         this._ipc.of[this._connectID].on('connect', () => {
-            this.emit('connect')
-        })
+            this.emit('connect');
+        });
 
         this._ipc.of[this._connectID].on('message', (data) => {
-            this.handleMessage(data.message)
+            this.handleMessage(data.message);
 
-            this.emit('receive', data.message)
-        })
+            this.emit('receive', data.message);
+        });
 
         this._ipc.of[this._connectID].on('disconnect', () => {
-            this.emit('close')
-        })
+            this.emit('close');
+        });
 
         this._ipc.of[this._connectID].on('error', (e) => {
             if (!e.message.startsWith('connect ECONNREFUSED')) {
-                console.log(e.name + ': ' + e.message)
+                console.log(e.name + ': ' + e.message);
             }
 
-            this.emit('error', e)
-        })
+            this.emit('error', e);
+        });
     }
 
     connect() {
         this._ipc.connectTo(this._connectID, () => {
-            this._addEventListeners()
-        })
+            this._addEventListeners();
+        });
     }
 
     disconnect() {
-        this._ipc.disconnect(this._ipc._connectID)
+        this._ipc.disconnect(this._ipc._connectID);
     }
 
     send(message) {
-        this._ipc.of[this._connectID].emit(
-            'message',
-            {
-                id: this._id,
-                message: message
-            }
-        )
+        this._ipc.of[this._connectID].emit('message', {
+            id: this._id,
+            message: message
+        });
 
-        this.emit('send', message)
+        this.emit('send', message);
     }
 }
 
-module.exports = IPCClient
+module.exports = IPCClient;
