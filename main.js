22f8087865cffa8e5de92f2f0a491abf3e1e8e49
bloof-bb <bloof.bb@gmail.com>
2017-01-14 23:03:14 -0600

    Removed menu from application

--- main.js
+++ main.js
@@ -1,20 +1,22 @@
-const electron = require('electron')
-const app = electron.app
-const BrowserWindow = electron.BrowserWindow
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
 
-const core = require('./core/core')
+const core = require('./core/core');
 
 app.on('ready', () => {
     let mainWindow = new BrowserWindow({
         width: 1200,
         height: 750,
-        show: false,
-        title: 'Flight View'
-    })
+        title: 'Flight View',
+        show: false
+    });
 
-    mainWindow.loadURL('file://' + __dirname + '/flight-view/index.html')
+    mainWindow.setMenu(null);
+
+    mainWindow.loadURL('file://' + __dirname + '/flight-view/index.html');
 
     mainWindow.once('ready-to-show', () => {
-        mainWindow.show()
-    })
-})
+        mainWindow.show();
+    });
+});
