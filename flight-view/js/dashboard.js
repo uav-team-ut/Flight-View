cb3470ea93afa733e4166c54b162d5d9be9d6196
bloof-bb <bloof.bb@gmail.com>
2017-01-23 16:07:00 -0600

    Created map directive in dashboard.js

---
+++ flight-view/js/dashboard.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const angular = require('angular');
+
+angular.module('flightView')
+    .directive('fvMap', () => {
+        return {
+            restrict: 'E',
+            template: 'TODO: make map work'
+        }
+    });
