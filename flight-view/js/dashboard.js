78061123f1a26e039ae6f5c84e89eccbde34cf04
bloof-bb <bloof.bb@gmail.com>
2017-01-29 01:38:13 -0600

    Added dynamic pitch markers

--- flight-view/js/dashboard.js
+++ flight-view/js/dashboard.js
@@ -12,6 +12,15 @@ angular.module('flightView')
     .directive('fvStatusBox', () => {
         return {
             restrict: 'E',
+                // function setScales(newValue, oldValue) {
+                //     $scope.pitchMinor = [-15, -5, 5, 15];
+                //     $scope.pitchMajor = [-20, -10, 10, 20];
+                // }
+                //
+                // $scope.$watch(($scope) => $scope.telemetry, setScales);
+                //
+                // setScales($scope.telemetry, $scope.telemetry);
+                //
             templateUrl: './templates/status-box.html'
         }
     });
