3f192b1cae536d4e9035553027223cca40d6e57c
bloof-bb <bloof.bb@gmail.com>
2017-02-04 20:38:33 -0600

    Added general layout to dashboard

--- flight-view/js/dashboard.js
+++ flight-view/js/dashboard.js
@@ -4,9 +4,26 @@ const angular = require('angular');
 
 angular.module('flightView')
     .directive('fvMap', () => {
+        let template = '<div id="mapbox-map" style=' +
+                '"position:absolute;top:0;bottom:0;left:0;right:0;"></div>';
+
         return {
             restrict: 'E',
-            template: 'TODO: make map work'
+            controller: ['$scope', ($scope) => {
+                $scope.$watch(() => document.getElementById('mapbox-map'),
+                        () => {
+                    const mapboxGL = require('mapbox-gl/dist/mapbox-gl');
+                    const Map = mapboxGL.Map;
+
+                    mapboxGL.accessToken = '<mapbox api key>';
+
+                    let map = new Map({
+                        container: 'mapbox-map',
+                        style: 'mapbox://styles/mapbox/satellite-v9'
+                    });
+                });
+            }],
+            template: template
         }
     })
     .directive('fvStatusBox', () => {
