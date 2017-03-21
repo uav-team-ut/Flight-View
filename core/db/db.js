c786969025c0c47555c3c68d4a7ced504d8d90a6
bloof-bb <bloof.bb@gmail.com>
2017-03-21 02:01:02 -0500

    Remove MapCacheDatastore

--- core/db/db.js
+++ core/db/db.js
@@ -2,13 +2,11 @@
 
 const TelemetryDatastore = require('./telemetry-datastore');
 const ImageDatastore = require('./image-datastore');
-const MapCacheDatastore = require('./map-cache-datastore');
 
 class Database {
     constructor(useArchives = true) {
         this.telemetry = new TelemetryDatastore(useArchives);
         this.images = new ImageDatastore(useArchives);
-        this.mapCache = new MapCacheDatastore();
     }
 }
 
--- util/types/types.js
+++ util/types/types.js
@@ -2,7 +2,6 @@
 
 module.exports = {
     Image: require('./image'),
-    MapCacheImage: require('./map-cache-image'),
     Target: require('./target'),
     Telemetry: require('./telemetry')
 };
