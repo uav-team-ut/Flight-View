e5756f5ee1ac72247728813c6f604b4d3ff032c7
bloof-bb <bloof.bb@gmail.com>
2017-04-09 21:51:38 -0500

    Handle incoming telemetry through REST api

--- core/host-server/routes/telemetry-router.js
+++ core/host-server/routes/telemetry-router.js
@@ -2,7 +2,19 @@
 
 const express = require('express');
 
+const parsers = require('../parsers');
+
+const Telemetry = require('../../../util/types').Telemetry;
+
 let router = express.Router();
+
+router.post('/', parsers.json, (req, res) => {
+    let telem = new Telemetry(req.body);
+
+    req.app.locals.telemetry.add(telem);
+
+    res.sendStatus(201);
+});
 
 router.get('/:time', (req, res) => {
     // return close time, option for splining.
--- core/host-server/telemetry.js
+++ core/host-server/telemetry.js
@@ -3,5 +3,12 @@
 module.exports = function Telemetry(server) {
     let telemetry = {};
 
+    telemetry.add = (telem) => {
+        server.coreSocket.send({
+            type: 'telemetry',
+            message: telem.serialize()
+        });
+    };
+
     return telemetry;
 };
