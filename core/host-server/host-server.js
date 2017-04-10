43a4f9bb4b4876e446694747b0da878be2839523
bloof-bb <bloof.bb@gmail.com>
2017-04-09 21:53:16 -0500

    Fix HostServer typos; allow closing

--- core/host-server/host-server.js
+++ core/host-server/host-server.js
@@ -16,14 +16,15 @@ const targetsRouter = require('./routes/targets-router');
 const telemetryRouter = require('./routes/telemetry-router');
 const timeRouter = require('./routes/time-router');
 
-const DEFAULT_PORT = 25005
+const DEFAULT_PORT = 25005;
 
 module.exports = class HostServer extends EventEmitter {
-    constructor(coreServer, coreSocker, allowListeners, port) {
+    constructor(coreServer, coreSocket, allowListeners, port) {
         super();
 
-        let serverPort = port || DEFAULT_PORT;
+        this._port = port || DEFAULT_PORT;
 
+        this._httpServer = null;
         this._app = express();
 
         this._app.use('/api/images', imagesRouter);
@@ -33,14 +34,20 @@ module.exports = class HostServer extends EventEmitter {
 
         this._app.locals.hostServer = this;
         this._app.locals.coreServer = coreServer;
-        this._app.locals.coreSocker = coreSocket;
+        this._app.locals.coreSocket = coreSocket;
         this._app.locals.db = new Database();
 
         this._app.locals.images = Images(this);
         this._app.locals.targets = Targets(this);
         this._app.locals.telemetry = Telemetry(this);
+    }
 
-        this._app.listen(serverPort);
+    listen() {
+        this._httpServer = this._app.listen(this._port);
+    }
+
+    stopListening() {
+        this._httpServer.close();
     }
 
     get coreServer() {
@@ -55,15 +62,15 @@ module.exports = class HostServer extends EventEmitter {
         return this._app.locals.db;
     }
 
-    get Images() {
+    get images() {
         return this._app.locals.images;
     }
 
-    get Targets() {
+    get targets() {
         return this._app.locals.targets;
     }
 
-    get Telemetry() {
+    get telemetry() {
         return this._app.locals.telemetry;
     }
 }
