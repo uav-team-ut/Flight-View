0c99f96e3fd308fc3f9c3682929061a090554342
bloof-bb <bloof.bb@gmail.com>
2017-04-16 15:41:08 -0500

    simplify image type, use unprocessed field

--- util/types/image.js
+++ util/types/image.js
@@ -4,29 +4,13 @@ const BaseType = require('./base-type');
 const units = require('./units');
 
 const IMAGE_FIELDS = {
-    data_original: {
+    file_original: {
         type: 'string',
         default: ''
     },
-    scale_original: {
-        type: units.PERCENTAGE
-    },
-    data_warped: {
+    file_warped: {
         type: 'string',
         default: ''
-    },
-    scale_warped: {
-        type: units.PERCENTAGE
-    },
-    has_warp: {
-        type: 'boolean',
-        default: false
-    },
-    time: {
-        type: units.TIME,
-        verify: (time) => {
-            return time >= 0;
-        }
     },
     lat: {
         type: units.ANGLE,
@@ -51,6 +35,10 @@ const IMAGE_FIELDS = {
         verify: (height) => {
             return height >= 0;
         }
+    },
+    processed: {
+        type: 'boolean',
+        default: false
     }
 };
 
