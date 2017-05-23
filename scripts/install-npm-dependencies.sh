1c63cd009b8c515d11e69341e7f63f9699cae7ea
jamesbellaero <jtb2013@gmail.com>
2017-05-23 15:16:56 -0500

    Create install-npm-dependencies.sh
    Things I had to install on Windows to get Electron to stop throwing errors.

---
+++ scripts/install-npm-dependencies.sh
@@ -0,0 +1,9 @@
+npm install npm@latest -g
+npm install dateformat --save
+npm install dotenv --save
+npm install nedb --save
+npm install sprintf-js --save
+npm install node-ipc --save
+npm install jimp --save
+
+
