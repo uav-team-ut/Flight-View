fbf5be6997e0ac1ed4598ab8c2e962088cd2545c
jamesbellaero <jtb2013@gmail.com>
2017-06-08 23:05:38 -0500

    Update images-router.js

--- core/host-server/routes/images-router.js
+++ core/host-server/routes/images-router.js
@@ -4,15 +4,51 @@ const express = require('express');
 
 const parsers = require('../parsers');
 
+const angular = require('angular');
 let router = express.Router();
 
 router.get('/', (req, res) => {
     // Return all images
     // add some options, like unprocessed
+    let img_limit=req.query.count;
+    let processed=req.query.processed;
+    let processed_manual=req.query.processed_manual;
+    if(angular.equals({},processed)==false&&processed==true){
+      if(angular.equals({},img_limit)==false){
+        res.send(req.app.locals.images.getUnprocessed(img_limit);
+      }
+      else
+      {
+        res.send(req.app.locals.images.getUnprocessed();
+      }
+    }
+    else if(angular.equals({},processed_manual)==false&&processed_manual==true){
+        if(angular.equals({},img_limit)==false){
+          res.send(req.app.locals.images.getUnprocessed(img_limit);
+        }
+        else
+        {
+          res.send(req.app.locals.images.getUnprocessed();
+        }
+    }
+    else{
+        if(angular.equals({},img_limit)==false){
+          res.send(req.app.locals.images.getUnprocessed(img_limit);
+        }
+        else
+        {
+          res.send(req.app.locals.images.getUnprocessed();
+        }
+    }
 });
 
 router.post('/', parsers.json, (req, res) => {
     // Post a new image
+    let img = new Image(req.body);
+
+    req.app.locals.image.add(img);
+
+    res.sendStatus(201);
 });
 
 router.get('/:id', (req, res) => {
