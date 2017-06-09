25410a8c1258c862e607cd3416f8afe8c00ed964
bloof-bb <bloof.bb@gmail.com>
2017-06-08 19:08:59 -0500

    Wrap auvsiClient target methods for REST api

--- core/host-server/routes/targets-router.js
+++ core/host-server/routes/targets-router.js
@@ -7,25 +7,68 @@ const parsers = require('../parsers');
 let router = express.Router();
 
 router.get('/', (req, res) => {
-    // Return all targets
-    //
-    // TODO: USE OPTION FOR NOT SENDING THE TARGET IMAGE TOO.
+    req.app.locals.auvsiClient.getTargets((error, targets) => {
+        if (error) res.sendStatus(500);
+        else res.send(targets);
+    });
 });
 
 router.post('/', parsers.json, (req, res) => {
-    // Add a new target
+    let targetNoData = req.body;
+
+    if (targetNoData.hasOwnProperty('data')) {
+        delete targetNoData.data;
+    }
+
+    req.app.locals.auvsiClient.postTarget(targetNoData, (error, target) => {
+        if (error) {
+            res.sendStatus(500);
+        } else if (req.body.hasOwnProperty('data')) {
+            req.app.locals.auvsiClient.putTargetImage(target.id, req.body.data,
+                    (error) => {
+                if (error) res.sendStatus(500);
+                else res.sendStatus(200);
+            });
+        } else {
+            res.sendStatus(200);
+        };
+    });
 });
 
 router.get('/:id', (req, res) => {
-    // Get a target with id
+    req.app.locals.auvsiClient.getTarget((error, target) => {
+        if (error) res.sendStatus(500);
+        else res.send(targets);
+    });
 });
 
-router.put('/:id', parsers.json, (req, res) => {
-    // Update a target
+router.patch('/:id', parsers.json, (req, res) => {
+    let targetNoData = req.body;
+
+    if (targetNoData.hasOwnProperty('data')) {
+        delete targetNoData.data;
+    }
+
+    req.app.locals.auvsiClient.putTarget(id, targetNoData, (error) => {
+        if (error) {
+            res.sendStatus(500);
+        } else if (req.body.hasOwnProperty('data')) {
+            req.app.locals.auvsiClient.putTargetImage(id, req.body.data,
+                    (error) => {
+                if (error) res.sendStatus(500);
+                else res.sendStatus(200);
+            });
+        } else {
+            res.sendStatus(200);
+        };
+    });
 });
 
 router.delete('/:id', (req, res) => {
-
+    req.app.locals.auvsiClient.deleteTarget(id, (error) => {
+        if (error) res.sendStatus(500);
+        else res.sendStatus(200);
+    });
 });
 
 module.exports = router;
